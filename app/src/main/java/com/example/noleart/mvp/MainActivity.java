41066d85ee8fcabafe17ce444789b3a8d652066d
niloleart <niloleart@gmail.com>
2017-03-01 15:15:05 +0100

    nav drawer added

--- app/src/main/java/com/example/noleart/mvp/MainActivity.java
+++ app/src/main/java/com/example/noleart/mvp/MainActivity.java
@@ -1,26 +1,26 @@
 package com.example.noleart.mvp;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Parcelable;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.noleart.mvp.api.database.Favourite;
 import com.example.noleart.mvp.api.database.Favourite_Table;
-import com.example.noleart.mvp.api.database.MyDatabase;
 import com.example.noleart.mvp.api.entities.Characters;
 import com.example.noleart.mvp.control.control.adapters.CharactersRecyclerViewAdapter;
 import com.example.noleart.mvp.control.control.callbacks.CharacterListCallback;
@@ -29,12 +29,9 @@ import com.example.noleart.mvp.views.interfaces.interfaces.MainContract;
 import com.example.noleart.mvp.views.interfaces.showcharacter.ShowCharacter;
 import com.example.noleart.mvp.views.interfaces.showcharacter.ShowFavCharacter;
 import com.google.gson.Gson;
-import com.raizlabs.android.dbflow.annotation.Table;
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
-import com.raizlabs.android.dbflow.sql.language.From;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
-import com.raizlabs.android.dbflow.sql.language.Select;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,8 +39,10 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static com.example.noleart.mvp.R.id.toolbar;
 
-public class MainActivity extends BaseActivity implements MainContract.View, CharacterListCallback {
+
+public class MainActivity extends BaseActivity implements MainContract.View, CharacterListCallback, NavigationView.OnNavigationItemSelectedListener {
     private final String TAG = "Main Activity";
     private static final String CHARACTER = "character";
     private static final String FAV_CHARACTER = "fav_character";
@@ -70,8 +69,13 @@ public class MainActivity extends BaseActivity implements MainContract.View, Cha
         FlowManager.init(new FlowConfig.Builder(this).build());
         ButterKnife.bind(this);
 
-        super.initToolbar();
+        Toolbar toolbar = super.initToolbar();
         setTitle("Inici");
+        initDrawer(toolbar);
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         presenter = new MainPresenterImpl();
         presenter.attach(this, this);
@@ -176,5 +180,57 @@ public class MainActivity extends BaseActivity implements MainContract.View, Cha
         }
 
     }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        /*if (id == R.id.nav_camera) {
+            Toast.makeText(this,"Camera pressed",Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }*/
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
--- app/src/main/java/com/example/noleart/mvp/views/interfaces/BaseActivity.java
+++ app/src/main/java/com/example/noleart/mvp/views/interfaces/BaseActivity.java
@@ -1,6 +1,9 @@
 package com.example.noleart.mvp.views.interfaces;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -12,7 +15,16 @@ import com.example.noleart.mvp.utils.Utils;
  */
 
 public class BaseActivity extends AppCompatActivity {
-    public void initToolbar() {
+    public void initDrawer(Toolbar toolbar){
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    public Toolbar initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
@@ -22,6 +34,7 @@ public class BaseActivity extends AppCompatActivity {
 
             setTitle(null);
         }
+        return toolbar;
     }
 
     public void initDetailToolbar() {
